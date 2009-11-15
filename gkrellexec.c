c9d7b2daef61f8c57173e9186cd8ed2a4bdd209f
Leandro Lisboa Penz <llpenz@gmail.com>
2009-11-15 16:21:16 -0200

    Plugin configuration saved, loaded and applied.

--- gkrellexec.c
+++ gkrellexec.c
@@ -10,6 +10,7 @@
 #include <gkrellm2/gkrellm.h>
 
 
+#define CONFIG_KEYWORD "gkrellexec"
 #define PLUGIN_PLACEMENT	MON_MAIL
 #define GKRELLEXEC_PROCESSES_MAXNUM 3
 
@@ -17,10 +18,10 @@
 
 
 static gchar AboutText[] =
-	"GKrellM Exec version 0\n\n\n"
-	"Copyright (C) 2009 by Leandro Penz\n"
-	"lpenz@terra.com.br\n"
-	"Released under the GPL\n";
+"GKrellM Exec version 0\n\n\n"
+"Copyright (C) 2009 by Leandro Penz\n"
+"lpenz@terra.com.br\n"
+"Released under the GPL\n";
 
 
 static struct
@@ -59,13 +60,13 @@ static void create_plugin_tab(GtkWidget * tab_vbox)
 	gtk_notebook_set_tab_pos(GTK_NOTEBOOK(tabs), GTK_POS_TOP);
 	gtk_box_pack_start(GTK_BOX(tab_vbox), tabs, TRUE, TRUE, 0);
 
-	for(i = 0; i < NMEMB(Config.processes); i++) {
+	for (i = 0; i < NMEMB(Config.processes); i++) {
 		GtkWidget *vbox0;
 		GtkWidget *hbox;
 		char tmp[256];
 
 		/* Tab: */
-		snprintf(tmp, sizeof tmp, "Process %d", i+1);
+		snprintf(tmp, sizeof tmp, "Process %d", i + 1);
 		vbox0 = gkrellm_gtk_framed_notebook_page(tabs, tmp);
 
 		/* Command line: */
@@ -90,15 +91,62 @@ static void create_plugin_tab(GtkWidget * tab_vbox)
 
 #if 0
 	// Info tab  ***
-    InfoLabel = gtk_label_new(amiconn_InfoText);
-    Label = gtk_label_new("Info");
-    gtk_notebook_append_page(GTK_NOTEBOOK(Tabs), InfoLabel, Label);
+	InfoLabel = gtk_label_new(amiconn_InfoText);
+	Label = gtk_label_new("Info");
+	gtk_notebook_append_page(GTK_NOTEBOOK(Tabs), InfoLabel, Label);
 #endif
 
 	/* About */
-    label = gtk_label_new("About");
-    about = gtk_label_new(AboutText);
-    gtk_notebook_append_page(GTK_NOTEBOOK(tabs), about, label);
+	label = gtk_label_new("About");
+	about = gtk_label_new(AboutText);
+	gtk_notebook_append_page(GTK_NOTEBOOK(tabs), about, label);
+}
+
+
+static void apply_plugin_config(void)
+{
+	int i;
+
+	for (i = 0; i < NMEMB(Config.processes); i++) {
+		snprintf(Config.processes[i].cmdline, sizeof(Config.processes[i].cmdline), "%s", gkrellm_gtk_entry_get_text(&Config.processes[i].cmdlineWidget));
+		Config.processes[i].timeout = atoi(gkrellm_gtk_entry_get_text(&Config.processes[i].timeoutWidget));
+	}
+
+#if 0
+	/* Re-layout pannel */
+	gkrellm_panel_destroy(amiconn_panel);
+	CreatePlugin(amiconn_vbox, TRUE);
+#endif
+}
+
+
+static void save_plugin_config(FILE *f)
+{
+	int i;
+
+	for (i = 0; i < NMEMB(Config.processes); i++) {
+		if (Config.processes[i].cmdline[0] == 0)
+			continue;
+		fprintf(f, CONFIG_KEYWORD " cmdline %d %s\n", i, Config.processes[i].cmdline);
+		fprintf(f, CONFIG_KEYWORD " timeout %d %d\n", i, Config.processes[i].timeout);
+	}
+}
+
+
+static void load_plugin_config(gchar *arg)
+{
+	gchar config[64], item[256];
+	gint n;
+	int i;
+
+	n = sscanf(arg, "%s %d %[^\n]", config, &i, item);
+	if (n == 3) {
+		if (strcmp(config, "cmdline") == 0)
+			snprintf(Config.processes[i].cmdline, sizeof(Config.processes[i].cmdline), "%s", item);
+		if (strcmp(config, "timeout") == 0)
+			sscanf(item, "%d\n", &Config.processes[i].timeout);
+		i++;
+	}
 }
 
 
@@ -108,19 +156,18 @@ static GkrellmMonitor  plugin_mon  =
 	0,              /* Id,  0 if a plugin           */
 	create_plugin,  /* The create_plugin() function */
 	update_plugin,  /* The update_plugin() function */
-	create_plugin_tab, /* The create_plugin_tab() config function */
-	NULL,           /* The apply_plugin_config() function      */
-	NULL,           /* The save_plugin_config() function  */
-	NULL,           /* The load_plugin_config() function  */
-	"gkrellexec",   /* config keyword                     */
+	create_plugin_tab,   /* The create_plugin_tab() config function */
+	apply_plugin_config, /* The apply_plugin_config() function      */
+	save_plugin_config,  /* The save_plugin_config() function  */
+	load_plugin_config,  /* The load_plugin_config() function  */
+	CONFIG_KEYWORD,      /* config keyword                     */
 	NULL,           /* Undefined 2  */
 	NULL,           /* Undefined 1  */
 	NULL,           /* Undefined 0  */
 	PLUGIN_PLACEMENT, /* Insert plugin before this monitor.       */
 	NULL,           /* Handle if a plugin, filled in by GKrellM */
 	NULL            /* path if a plugin, filled in by GKrellM   */
-}
-;
+};
 
 
 GkrellmMonitor *gkrellm_init_plugin(void)
